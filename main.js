07ea8913e0f0e587aa95f55903d8863c221d18dc
Yoshinori Kanno <yoshinori.kanno@me.com>
2023-07-21 18:13:16 +0900

    ✨ Introduce new features.

--- main.js
+++ main.js
@@ -2,24 +2,20 @@ const articleContents = document.querySelector(".article-contents");
 const asideContents = document.querySelector(".aside-contents");
 const aside = document.querySelector("aside");
 const navClinentHeight = document.querySelector("nav").clientHeight;
-console.log(navClinentHeight)
-console.log(aside.getBoundingClientRect().bottom - 20);
-
-// スクロールイベントとリサイズイベントで
+const asideContentsWidth = asideContents.clientWidth;
 window.addEventListener("scroll", function () {
-  // .aside-contents の左上がウィンドウトップからの位置を取得してください
-  console.log(asideContents.getBoundingClientRect().top);
   asideContents.style.position = "";
-  if (asideContents.getBoundingClientRect().top < navClinentHeight + 20) {
-    // .aside-contents に style position fixed を付与してください
-
+  if (asideContents.getBoundingClientRect().top <= navClinentHeight + 20 && asideContents.clientHeight + navClinentHeight + 20 <= articleContents.getBoundingClientRect().bottom ) {
     asideContents.style.position = "fixed";
     asideContents.style.top = navClinentHeight + 20 + "px";
+    asideContents.style.width = asideContentsWidth + "px";
+  } if ( asideContents.clientHeight + navClinentHeight + 20 >= articleContents.getBoundingClientRect().bottom ){
+    aside.style.display = 'flex'
+    aside.style.alignItems = 'flex-end'
   } else {
-    // .aside-contents に style position を空にしてください
-    asideContents.style.position = "";
+    aside.style.display = 'unset'
+    aside.style.alignItems = 'unset'
   }
-
 })
 
 
